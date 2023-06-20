bcacda76694988d623a6dde6eb9dc1b3c41bddf0
Scheppsi <81817998+Scheppsi@users.noreply.github.com>
2023-06-20 21:16:55 +0200

    Cleanup Job

--- src/app/export-xml/export-xml.component.ts
+++ src/app/export-xml/export-xml.component.ts
@@ -12,17 +12,7 @@ import { XmlMapperService } from '../xml-mapper.service';
 export class ExportXmlComponent {
 
   constructor(private http: HttpClient, private xmlMapperService: XmlMapperService) {}
-
-  // fetchData() {
-  //   this.http.get('api/output').subscribe(data => {
-  //     console.log(data);
-  //     this.jObj = data;
-  //   });
-  //   console.log(this.jObj);
-    
-  //   return this.jObj;
-  // }
-
+  
   fetchData() {
     this.http.get('api/output',{responseType:'text'}).subscribe(data => {
       const originalString = (data as string);
@@ -32,8 +22,15 @@ export class ExportXmlComponent {
         var result = convert.js2xml(this.xmlMapperService.mapJsontoXmlrdyJson(test), options);
         console.log(result);
         this.result = result;
-
     });
+    const blob = new Blob([this.result], { type: 'application/xml' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'export.xml';
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
   }
 
   result: string = '';
@@ -49,173 +46,3 @@ export class ExportXmlComponent {
     a.remove();
   }
 }
-// {
-//   "input": {
-//       "qualitycontrol": {
-//           "_attributes": {
-//               "type": "no",
-//               "losequantity": "0",
-//               "delay": "0"
-//           }
-//       },
-//       "sellwish": {
-//           "item": [
-//               {
-//                   "_attributes": {
-//                       "article": "1",
-//                       "quantity": "150"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "2",
-//                       "quantity": "250"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "3",
-//                       "quantity": "100"
-//                   }
-//               }
-//           ]
-//       },
-//       "selldirect": {
-//           "item": [
-//               {
-//                   "_attributes": {
-//                       "article": "1",
-//                       "quantity": "0",
-//                       "price": "0.0",
-//                       "penalty": "0.0"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "2",
-//                       "quantity": "0",
-//                       "price": "0.0",
-//                       "penalty": "0.0"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "3",
-//                       "quantity": "0",
-//                       "price": "0.0",
-//                       "penalty": "0.0"
-//                   }
-//               }
-//           ]
-//       },
-//       "orderlist": {
-//           "order": [
-//               {
-//                   "_attributes": {
-//                       "article": "22",
-//                       "quantity": "300",
-//                       "modus": "3"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "27",
-//                       "quantity": "1800",
-//                       "modus": "5"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "28",
-//                       "quantity": "1000",
-//                       "modus": "5"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "32",
-//                       "quantity": "1500",
-//                       "modus": "3"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "33",
-//                       "quantity": "400",
-//                       "modus": "3"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "38",
-//                       "quantity": "520",
-//                       "modus": "3"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "39",
-//                       "quantity": "550",
-//                       "modus": "5"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "40",
-//                       "quantity": "450",
-//                       "modus": "5"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "41",
-//                       "quantity": "650",
-//                       "modus": "3"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "42",
-//                       "quantity": "900",
-//                       "modus": "5"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "44",
-//                       "quantity": "2500",
-//                       "modus": "3"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "45",
-//                       "quantity": "250",
-//                       "modus": "5"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "46",
-//                       "quantity": "250",
-//                       "modus": "5"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "57",
-//                       "quantity": "630",
-//                       "modus": "3"
-//                   }
-//               },
-//               {
-//                   "_attributes": {
-//                       "article": "58",
-//                       "quantity": "12500",
-//                       "modus": "3"
-//                   }
-//               }
-//           ]
-//       },
-//       "productionlist": {
-
